2875e20561e493f11dc643e2848b8e5b4ea8ec64
raghav-kukreti <raghav.kukreti@gmail.com>
2017-12-07 20:07:27 +0530

    UPDATED Home

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $("#nav-icon1").on("click", function() {
 	$(this).toggleClass("open");
-	if ($("main").hasClass("animated fadeIn")) {
-		$("main").removeClass("animated fadeIn");
-		$("main").addClass("animated fadeOut");
+	if ($(".nav").hasClass("animated fadeIn")) {
+		$(".nav").removeClass("animated fadeIn");
+		$(".nav").addClass("animated fadeOut");
 	}
 	else {
-		$("main").removeClass("animated fadeOut");
-    $("main").addClass("animated fadeIn");
+		$(".nav").removeClass("animated fadeOut");
+    $(".nav").addClass("animated fadeIn");
 	}
 });
