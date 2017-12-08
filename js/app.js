7f1dbd137754280feb59668a3c1374281f7a239d
raghav-kukreti <raghav.kukreti@gmail.com>
2017-12-08 12:03:10 +0530

    Animations and shit + additional stuff

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $("#nav-icon1").on("click", function() {
 	$(this).toggleClass("open");
+	$(".nav").toggleClass("imp_z")
 	if ($(".nav").hasClass("animated fadeIn")) {
 		$(".nav").removeClass("animated fadeIn");
 		$(".nav").addClass("animated fadeOut");
