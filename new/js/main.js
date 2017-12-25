915c157547f8c41a9879610e15cc647700009491
raghav-kukreti <raghav.kukreti@gmail.com>
2017-12-25 13:14:49 +0530

    Major updates

--- new/js/main.js
+++ new/js/main.js
@@ -547,7 +547,7 @@
 
 	// Preload all the images.
 	imagesLoaded(DOM.scroller, function() {
-		var extradelay = 1000;
+		var extradelay = 0;
 		// Slide out loader.
 		anime({
 			targets: DOM.loader,
